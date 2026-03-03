3cc59736387e3e71d229559750a950e14a82cdf1
Mian Muhammad Waleed <mianwaleed689@gmail.com>
2026-03-03 23:20:56 +0400

    Add temporary price fix tool

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import EmaarDashboardV2 from "./EmaarDashboardV2";
 import AdminPanel from "./AdminPanel";
 import ProjectManager from "./ProjectManager";
-
+import FixPrices from "./FixPrices";
 function App() {
   return (
     <BrowserRouter>
@@ -11,9 +11,16 @@ function App() {
         <Route path="/" element={<EmaarDashboardV2 />} />
         <Route path="/admin" element={<AdminPanel />} />
         <Route path="/manage" element={<ProjectManager />} />
+        <Route path="/fix-prices" element={<FixPrices />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
 export default App;
+```
+
+Then deploy:
+```
+git add -A
+git commit -m "Add temporary price fix tool"
+git push
