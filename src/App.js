19b4ccc533e2195578714a76f0599d8875e44bcb
Mian Muhammad Waleed <mianwaleed689@gmail.com>
2026-03-03 07:03:22 +0400

    project manager editor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from "react"; 
-import { BrowserRouter, Routes, Route } from "react-router-dom"; 
-import EmaarDashboardV2 from "./EmaarDashboardV2"; 
-import AdminPanel from "./AdminPanel"; 
-import ProjectManager from "./ProjectManager"; 
-function App() { return ( 
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import EmaarDashboardV2 from "./EmaarDashboardV2";
+import AdminPanel from "./AdminPanel";
+import ProjectManager from "./ProjectManager";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<EmaarDashboardV2 />} />
+        <Route path="/admin" element={<AdminPanel />} />
+        <Route path="/manage" element={<ProjectManager />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
