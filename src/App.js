5489cc18051678a41ee4ddf149f241a9d27e80c3
Mian Muhammad Waleed <mianwaleed689@gmail.com>
2026-03-04 00:03:28 +0400

    Remove temporary fix-prices tool

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import EmaarDashboardV2 from "./EmaarDashboardV2";
 import AdminPanel from "./AdminPanel";
 import ProjectManager from "./ProjectManager";
-import FixPrices from "./FixPrices";
 function App() {
   return (
     <BrowserRouter>
@@ -11,9 +10,15 @@ function App() {
         <Route path="/" element={<EmaarDashboardV2 />} />
         <Route path="/admin" element={<AdminPanel />} />
         <Route path="/manage" element={<ProjectManager />} />
-        <Route path="/fix-prices" element={<FixPrices />} />
       </Routes>
     </BrowserRouter>
   );
 }
 export default App;
+```
+
+Save, then:
+```
+git add -A
+git commit -m "Remove temporary fix-prices tool"
+git push
