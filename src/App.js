b7f8c96df3e2ef734018fb020da24fe6247421d0
Mian Muhammad Waleed <mianwaleed689@gmail.com>
2026-03-03 06:59:14 +0400

    fix manage route to ProjectManager

--- src/App.js
+++ src/App.js
@@ -1,19 +1,6 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import EmaarDashboardV2 from "./EmaarDashboardV2";
-import AdminPanel from "./AdminPanel";
-import SeedData from "./SeedData";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<EmaarDashboardV2 />} />
-        <Route path="/admin" element={<AdminPanel />} />
-        <Route path="/manage" element={<SeedData />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
+import React from "react"; 
+import { BrowserRouter, Routes, Route } from "react-router-dom"; 
+import EmaarDashboardV2 from "./EmaarDashboardV2"; 
+import AdminPanel from "./AdminPanel"; 
+import ProjectManager from "./ProjectManager"; 
+function App() { return ( 
