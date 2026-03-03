010edd034014170a9da6dcdb26e15c1370b35276
Mian Muhammad Waleed <mianwaleed689@gmail.com>
2026-03-03 06:36:48 +0400

    add manage route back

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import EmaarDashboardV2 from "./EmaarDashboardV2";
 import AdminPanel from "./AdminPanel";
+import SeedData from "./SeedData";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<EmaarDashboardV2 />} />
         <Route path="/admin" element={<AdminPanel />} />
+        <Route path="/manage" element={<SeedData />} />
       </Routes>
     </BrowserRouter>
   );
