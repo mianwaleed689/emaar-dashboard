0b9b24b5074b3ece38e03620ba1860cb0121f4e7
Mian Muhammad Waleed <mianwaleed689@gmail.com>
2026-03-04 05:15:58 +0400

    Admin sidebar links and design overhaul

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import EmaarDashboardV2 from "./EmaarDashboardV2";
 import AdminPanel from "./AdminPanel";
 import ProjectManager from "./ProjectManager";
+import LandingPage from "./LandingPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<EmaarDashboardV2 />} />
         <Route path="/admin" element={<AdminPanel />} />
         <Route path="/manage" element={<ProjectManager />} />
+        <Route path="/landing" element={<LandingPage onLoginClick={() => window.location.href = "/"} onSignUpClick={() => window.location.href = "/"} />} />
       </Routes>
     </BrowserRouter>
   );
