45a27a64dd37791ef7097dd827c2b122b9aa947a
Mian Muhammad Waleed <mianwaleed689@gmail.com>
2026-03-29 16:47:58 +0400

    fix: remove conflicting tab imports - keep only BillingTab ReferralTab ForecastingTab PricingPlansTab MarketIntelligenceTab

--- src/AdminPanel.jsx
+++ src/AdminPanel.jsx
@@ -12,17 +12,17 @@ import { emaarProjects, emaarCommunities, emaarYields, communityROI as defaultCo
 import ProjectManager from "./ProjectManager";
 import { useI18n, LANGUAGES } from "./i18n";
 import AdminDataHealth from "./AdminDataHealth";
-import EmailCampaignsTab from "./admin/EmailCampaignsTab";
+
 import MarketIntelligenceTab from "./admin/MarketIntelligenceTab";
 import ForecastingTab from "./admin/ForecastingTab";
 import PricingPlansTab from "./admin/PricingPlansTab";
 import BillingTab from "./admin/BillingTab";
 import ReferralTab from "./admin/ReferralTab";
-import NotificationsTab from "./admin/NotificationsTab";
-import DigestTab from "./admin/DigestTab";
-import EiborRatesPanel from "./admin/EiborRatesPanel";
-import UsersTab from "./admin/UsersTab";
-import DataManagerTab from "./admin/DataManagerTab";
+
+
+
+
+
 
 /* ═══════════════════════════════════════════════════════
    EMAIL CAMPAIGNS TAB
